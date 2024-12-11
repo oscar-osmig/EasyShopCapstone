ca69522525a100ba29489bf1f282f8323e65ee0b
Osmig_Torres <torresosmig@gmail.com>
2024-12-11 15:19:42 -0800

    initial commit

---
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,100 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
+
+// add the annotations to make this a REST controller
+// add the annotation to make this controller the endpoint for the following url
+    // http://localhost:8080/categories
+// add annotation to allow cross-site origin requests
+@RestController
+@CrossOrigin
+@PreAuthorize("PermitAll()")
+public class CategoriesController
+{
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
+
+    // create an Autowired controller to inject the categoryDao and ProductDao
+
+    // add the appropriate annotation for a get action
+    @PreAuthorize("permitAll()")
+    @GetMapping("categories")
+    public List<Category> getAll()
+    {
+        // find and return all categories
+        return categoryDao.getAllCategories();
+
+    }
+
+    // add the appropriate annotation for a get action
+    @GetMapping("{id}")
+    @PreAuthorize("permitAll()")
+    public Category getById(@PathVariable int id)
+    {
+        // get the category by id
+        return categoryDao.getById(id);
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return null;
+    }
+
+    // add annotation to call this method for a POST action
+    // add annotation to ensure that only an ADMIN can call this function
+    public Category addCategory(@RequestBody Category category)
+    {
+        // insert the category
+        return null;
+    }
+
+    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        // update the category by id
+    }
+
+
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public void deleteCategory(@PathVariable int id)
+    {
+        // delete the category by id
+    }
+
+    public CategoryDao getCategoryDao() {
+        return categoryDao;
+    }
+
+    public void setCategoryDao(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    public ProductDao getProductDao() {
+        return productDao;
+    }
+
+    public void setProductDao(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+}
---
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -0,0 +1,93 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.CategoryDao;
+import org.yearup.models.Category;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
+{
+    public MySqlCategoryDao(DataSource dataSource)
+    {
+        super(dataSource);
+    }
+
+
+
+    @Override
+    public List<Category> getAllCategories()
+    {
+        List<Category> categories = new ArrayList<>();
+
+        String sql = "SELECT * FROM categories";
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet row = statement.executeQuery();
+
+            while (row.next())
+            {
+                Category category = mapRow(row);
+                categories.add(category);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        return categories;
+    }
+
+    @Override
+    public Category getById(int categoryId)
+    {
+        // get category by id
+        return null;
+    }
+
+    @Override
+    public Category create(Category category)
+    {
+        // create a new category
+        return null;
+    }
+
+    @Override
+    public void update(int categoryId, Category category)
+    {
+        // update category
+    }
+
+    @Override
+    public void delete(int categoryId)
+    {
+        // delete category
+    }
+
+    private Category mapRow(ResultSet row) throws SQLException
+    {
+        int categoryId = row.getInt("category_id");
+        String name = row.getString("name");
+        String description = row.getString("description");
+
+        Category category = new Category()
+        {{
+            setCategoryId(categoryId);
+            setName(name);
+            setDescription(description);
+        }};
+
+        return category;
+    }
+
+}
